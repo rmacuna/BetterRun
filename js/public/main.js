ed88c8e99192cfec736d9d97ba113b4fd9b06e6a
Roberto <rmacuna@uninorte.edu.co>
2017-10-02 22:48:13 -0500

    Cambio de pagina sin recargar con ajax

--- js/public/main.js
+++ js/public/main.js
@@ -1,16 +1,18 @@
-
 function showModal() {
     $('.ui.modal')
-        .modal('show');}
+        .modal('show');
+}
 
-function playClickSound(){
-	var audio = document.getElementById('audioPlay');
-	audio.play();}
+function playClickSound() {
+    var audio = document.getElementById('audioPlay');
+    audio.play();
+    $("#body-content").load('../public/mainmenu.html');
+}
 
-$(".hoverableSound").mouseenter(function(){
-  	$("<audio></audio>").attr({
-  		'src':'../assets/sounds/hoverClick.mp3',
-  		'volume':0.4,
-  		'autoplay':'autoplay'
-  	}).appendTo("body");
+$(".hoverableSound").mouseenter(function() {
+    $("<audio></audio>").attr({
+        'src': '../assets/sounds/hoverClick.mp3',
+        'volume': 0.4,
+        'autoplay': 'autoplay'
+    }).appendTo("body");
 });
