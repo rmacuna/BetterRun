cfc3c77b01ce5d3acbd85fb017961900cd225041
Roberto <rmacuna@uninorte.edu.co>
2017-10-02 19:29:45 -0500

    Terminado pagina principal, cuando tenga los dibujos de los personajes se colocaran

--- js/public/main.js
+++ js/public/main.js
@@ -6,4 +6,11 @@ function showModal() {
 function playClickSound(){
 	var audio = document.getElementById('audioPlay');
 	audio.play();}
-	
+
+$(".hoverableSound").mouseenter(function(){
+  	$("<audio></audio>").attr({
+  		'src':'../assets/sounds/hoverClick.mp3',
+  		'volume':0.4,
+  		'autoplay':'autoplay'
+  	}).appendTo("body");
+});
