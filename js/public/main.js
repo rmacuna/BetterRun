3bb9a764bad9fc0ed2127f09c6e2ff8104c256d9
Roberto <rmacuna@uninorte.edu.co>
2017-10-08 17:20:42 -0500

    Integrado la aplicacion con type.js que me permite escribir animadamente en la pantalla, asi como fue integrado  un diseo base del menu principal del mapa. Los niveles fueron cuidadosamente filtrados y eliminados algunos que no eran necesarios, se redujo el peso de la aplicacion comprimiento imagenes

--- js/public/main.js
+++ js/public/main.js
@@ -1,3 +1,7 @@
+function selectedGameMode() {
+
+}
+
 function showModal() {
     $('.ui.modal')
         .modal('show');
@@ -6,26 +10,32 @@ function showModal() {
 function playGame() {
     var audio = document.getElementById('audioPlay');
     audio.play();
-    var millisecondsToWait = 800;  
-      
-    setTimeout(function() {
-       $("#body-content").load('../public/mainmenu.html');
-       $(document).ready(function() {
-   			 setTimeout(function(){
-   				 $('body').addClass('loaded')
-   			 },3000);
-   		});
-    }, millisecondsToWait);
-      
-}
-var hello = "helloWord";
-console.log(hello);
+    var millisecondsToWait = 2000;
+    $(document).ready(function () {
+       $('body').addClass('vanish') 
+    });
+        setTimeout(function () {
+            $("#body-content").load('../public/mainmenu.html');
+            $(document).ready(function () {
+                $('body').addClass('visiblePage')
+                $('body').removeClass('vanish')
+                
+                setTimeout(function () {
+                   
+                    $('body').addClass('loaded')
+                    $('body').removeClass('visiblePage')
+                }, 3000);
+            });
+        }, millisecondsToWait);
 
-$(".hoverableSound").mouseenter(function() {
-    $("<audio></audio>").attr({
-        'src': '../assets/sounds/hoverClick.mp3',
-        'volume': 0.4,
-        'autoplay': 'autoplay'
-    }).appendTo("body");
-});
+    }
+$(".hoverableSound").mouseenter(function () {
+            $("<audio></audio>").attr({
+                'src': '../assets/sounds/hoverClick.mp3',
+                'volume': 0.4,
+                'autoplay': 'autoplay'
+            }).appendTo("body");
+        });
+
+
 
