8f731e12d273d1e075e9ae3192e2e7349628f77f
Roberto <rmacuna@uninorte.edu.co>
2017-10-09 08:44:25 -0500

    Agregado efectos en el mainmenu y conectado con el index jugable. Hay un problema y es que no detecta el abrir un modal en el mainmenu por razones que aun desconosco.

--- js/public/main.js
+++ js/public/main.js
@@ -6,36 +6,36 @@ function showModal() {
     $('.ui.modal')
         .modal('show');
 }
-
 function playGame() {
     var audio = document.getElementById('audioPlay');
     audio.play();
     var millisecondsToWait = 2000;
     $(document).ready(function () {
-       $('body').addClass('vanish') 
+        $('body').addClass('vanish')
     });
-        setTimeout(function () {
-            $("#body-content").load('../public/mainmenu.html');
-            $(document).ready(function () {
-                $('body').addClass('visiblePage')
-                $('body').removeClass('vanish')
-                
-                setTimeout(function () {
-                   
-                    $('body').addClass('loaded')
-                    $('body').removeClass('visiblePage')
-                }, 3000);
-            });
-        }, millisecondsToWait);
+    setTimeout(function () {
+        $("#body-content").load('../public/mainmenu.html');
+        $(document).ready(function () {
+            $('body').addClass('visiblePage')
+            $('body').removeClass('vanish')
 
-    }
-$(".hoverableSound").mouseenter(function () {
-            $("<audio></audio>").attr({
-                'src': '../assets/sounds/hoverClick.mp3',
-                'volume': 0.4,
-                'autoplay': 'autoplay'
-            }).appendTo("body");
+            setTimeout(function () {
+
+                $('body').addClass('loaded')
+                $('body').removeClass('visiblePage')
+            }, 3000);
         });
+    }, millisecondsToWait);
+
+}
+
+$(".hoverableSound").mouseenter(function () {
+    $("<audio></audio>").attr({
+        'src': '../assets/sounds/hoverClick.mp3',
+        'volume': 0.4,
+        'autoplay': 'autoplay'
+    }).appendTo("body");
+});
 
 
 
