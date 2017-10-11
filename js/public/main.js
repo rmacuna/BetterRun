cc1ab882458dea55ecaa3fcf4600009c98097556
Roberto <rmacuna@uninorte.edu.co>
2017-10-10 22:48:57 -0500

    Conectado con seleccion de mapas el menu principal del juego, ya puedes abrir una ventana donde seleccionas el mapa que quieres jugar

--- js/public/main.js
+++ js/public/main.js
@@ -1,25 +1,26 @@
-function selectedGameMode() {
-
-}
-
 function showModal() {
     $('.ui.modal')
         .modal('show');
 }
+
+function play() {
+    $('#body-content').load('../public/mapselect.html');
+}
+
 function playGame() {
     var audio = document.getElementById('audioPlay');
     audio.play();
     var millisecondsToWait = 2000;
-    $(document).ready(function () {
+    $(document).ready(function() {
         $('body').addClass('vanish')
     });
-    setTimeout(function () {
+    setTimeout(function() {
         $("#body-content").load('../public/mainmenu.html');
-        $(document).ready(function () {
+        $(document).ready(function() {
             $('body').addClass('visiblePage')
             $('body').removeClass('vanish')
 
-            setTimeout(function () {
+            setTimeout(function() {
 
                 $('body').addClass('loaded')
                 $('body').removeClass('visiblePage')
@@ -29,13 +30,18 @@ function playGame() {
 
 }
 
-$(".hoverableSound").mouseenter(function () {
+function selectMap(id) {
+    $('.map-shape').mousedown(function(id) {
+        $(id).addClass('selectedMap')
+        console.log('ds')
+    });
+}
+
+
+$(".hoverableSound").mouseenter(function() {
     $("<audio></audio>").attr({
         'src': '../assets/sounds/hoverClick.mp3',
         'volume': 0.4,
         'autoplay': 'autoplay'
     }).appendTo("body");
 });
-
-
-
