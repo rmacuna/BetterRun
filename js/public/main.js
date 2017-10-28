aef0beb7c6e9f4bb0579200059d93c40a1d66110
Roberto <rmacuna@uninorte.edu.co>
2017-10-28 10:31:36 -0500

    Agregado pistas de audio y musica para el juego, sin embargo hay que reducir y comprimirlas para así poder optimizar el espacio que ocupa el juego

--- js/public/main.js
+++ js/public/main.js
@@ -1,40 +1,47 @@
 // Remueve la propiedad de que un imagen pueda ser arrastrada de la pantalla
 window.ondragstart = function() { return false; }
+
 var lastMap = 0;
 
 var maps = [
         {
             "name": "El bosque",
+            "audio": "../assets/sound/jungle.mp3",
             "image": "../assets/levels/forest_level.png",
             "blurImage": "../assets/images/forest_level_blur.jpg",
             "description": "El bosque encantado es una locacion bastante sencilla, no hubo nunca un lugar tan calmado como este"
         },
         {
             "name": "El desierto",
+            "audio": "",
             "image": "../assets/levels/Desert_level.png",
             "blurImage": "../assets/images/Desert_level_blur.jpg",
             "description": "El desierto es aterrador para muchos, pero la verdad es que este parece un oasis tropical. No confíes en el agua que llueve aquí..."
         },
         {
             "name": "Cementerio",
+            "audio": "",
             "image": "../assets/levels/Cementery_level.png",
             "blurImage": "../assets/images/Cementery_level_blur.jpg",
             "description": "El cementerio es un lugar aterrador, quien sabe cuantos fantasmas salen por aquí"
         },
         {
             "name": "Base Militar Luna",
+            "audio": "",
             "image": "../assets/levels/moonland_level.jpg",
             "blurImage": "../assets/images/moonland_level_blur.jpg",
             "description": "En la luna hay que tener mucho cuidado, la gravedad es diferente y ultimamente caen rocas espaciales"
         },
         {
             "name": "Cueva de hielo",
+            "audio": "",
             "image": "../assets/levels/ice_level.jpg",
             "blurImage":  "../assets/images/ice_level_blur.jpg",
             "description": "HACE DEMASIADO FRÍO AQUÍ, sin mencionar que de la cueva caen bloques de hielo mortales"
         },
         {
             "name": "La ciudad perdida",
+            "audio": "",
             "image": "../assets/levels/libertalia_level.jpg",
             "blurImage": "../assets/images/libertalia_level-blur.jpg",
             "description": "La ciudad perdida es un lugar perdido, visitandolo depronto descubras donde esta y te vuelvas famoso"
@@ -42,7 +49,6 @@ var maps = [
 ];
 
 function play() {
-    var audio = document
     $('#body-content').load('../public/mapselec.html');  
 }
  
@@ -90,6 +96,12 @@ function about (argument) {
 }
 
 function next() {
+
+    var audio = document.getElementById('audioPassLevel');
+    audio.volume = 0.2;
+    audio.load();
+    audio.play();
+
     if (lastMap == 0 || lastMap < maps.length - 1) {
         var url = maps[lastMap + 1].blurImage;
         var miniatura = maps[lastMap + 1].image;
@@ -110,6 +122,12 @@ function next() {
 }
 
 function prev() {
+
+    var audio = document.getElementById('audioPassLevel');
+    audio.volume = 0.2;
+    audio.load();
+    audio.play();
+
     if (lastMap == 0) {
         console.log('inicio vamonos a devolvernos al final')
         lastMap = maps.length - 1;
@@ -131,12 +149,14 @@ function prev() {
 }
 
 function playGame() {
+    var backgroundAudio = document.getElementById('#BetterRun');
     var audio = document.getElementById('audioPlay');
     audio.play();
+    console.log(backgroundAudio)
     var millisecondsToWait = 2000;
-    
     $(document).ready(function() {
-             $('body').addClass('vanish')
+        $('body').addClass('vanish')
+
     });
     setTimeout(function() {
         $("#body-content").load('../public/mapselect.html');
@@ -151,7 +171,6 @@ function playGame() {
     }, millisecondsToWait);
 }
 
-
 function showModal() {
     $('.ui.modal')
         .modal('show');
@@ -160,7 +179,7 @@ function showModal() {
 $(".hoverableSound").mouseenter(function() {
     $("<audio></audio>").attr({
         'src': '../assets/sounds/hoverClick.mp3',
-        'volume': 0.4,
+        'volume': 0.2,
         'autoplay': 'autoplay'
     }).appendTo("body");
 });
