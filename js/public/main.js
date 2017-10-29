f703c7e149a181e0df529e23cc7a708ceea82075
Roberto <rmacuna@uninorte.edu.co>
2017-10-29 13:56:24 -0500

    Incluido sonidos a los mapas al moverse en el menu de seleccion de mapas

--- js/public/main.js
+++ js/public/main.js
@@ -1,40 +1,43 @@
 // Remueve la propiedad de que un imagen pueda ser arrastrada de la pantalla
 window.ondragstart = function() { return false; }
-
 var lastMap = 0;
-
 var maps = [
         {
             "name": "El bosque",
-            "audio": "../assets/sound/jungle.mp3",
+            "music": "../assets/music/BetterRun.mp3",
+            "soundEffect": "../assets/sounds/forest.mp3",
             "image": "../assets/levels/forest_level.png",
             "blurImage": "../assets/images/forest_level_blur.jpg",
             "description": "El bosque encantado es una locacion bastante sencilla, no hubo nunca un lugar tan calmado como este"
         },
         {
             "name": "El desierto",
-            "audio": "",
+            "audio": "../assets/music/DesertMap.mp3",
+            "soundEffect": "../assets/sounds/ancient_egyptian.mp3",
             "image": "../assets/levels/Desert_level.png",
             "blurImage": "../assets/images/Desert_level_blur.jpg",
             "description": "El desierto es aterrador para muchos, pero la verdad es que este parece un oasis tropical. No confíes en el agua que llueve aquí..."
         },
         {
             "name": "Cementerio",
-            "audio": "",
+            "audio": "../assets/music/HalloweenMap.mp3",
+            "soundEffect": "../assets/sounds/horror_amb.mp3",
             "image": "../assets/levels/Cementery_level.png",
             "blurImage": "../assets/images/Cementery_level_blur.jpg",
             "description": "El cementerio es un lugar aterrador, quien sabe cuantos fantasmas salen por aquí"
         },
         {
             "name": "Base Militar Luna",
-            "audio": "",
+            "audio": "../assets/music/SpaceMap.mp3",
+            "soundEffect": "../assets/sounds/space_amb.mp3",
             "image": "../assets/levels/moonland_level.jpg",
             "blurImage": "../assets/images/moonland_level_blur.jpg",
             "description": "En la luna hay que tener mucho cuidado, la gravedad es diferente y ultimamente caen rocas espaciales"
         },
         {
             "name": "Cueva de hielo",
-            "audio": "",
+            "audio": "../assets/music/WinterMap.mp3",
+            "soundEffect": "../assets/sounds/wind.mp3",
             "image": "../assets/levels/ice_level.jpg",
             "blurImage":  "../assets/images/ice_level_blur.jpg",
             "description": "HACE DEMASIADO FRÍO AQUÍ, sin mencionar que de la cueva caen bloques de hielo mortales"
@@ -42,6 +45,7 @@ var maps = [
         {
             "name": "La ciudad perdida",
             "audio": "",
+            "soundEffect": "../assets/sounds/jungle.mp3",
             "image": "../assets/levels/libertalia_level.jpg",
             "blurImage": "../assets/images/libertalia_level-blur.jpg",
             "description": "La ciudad perdida es un lugar perdido, visitandolo depronto descubras donde esta y te vuelvas famoso"
@@ -98,61 +102,101 @@ function about (argument) {
 function next() {
 
     var audio = document.getElementById('audioPassLevel');
+    document.getElementById('sound').innerHTML = "";
+
     audio.volume = 0.2;
     audio.load();
     audio.play();
 
     if (lastMap == 0 || lastMap < maps.length - 1) {
+
         var url = maps[lastMap + 1].blurImage;
         var miniatura = maps[lastMap + 1].image;
+        var src = maps[lastMap + 1].soundEffect;
+
         document.body.style.width = "100%";
         document.body.style.background = "url( " + url + ") no-repeat center center fixed";
         document.getElementById("mapimg").src = miniatura;
         document.body.style.backgroundSize = "cover";
+        var sound = document.getElementById('sound');
+        htmlString = "<audio " + "id='mapsound' " + "src=" + src + "></audio>";
+        sound.insertAdjacentHTML('beforeend', htmlString);
+        audio = document.getElementById('mapsound');
+        audio.load();
+        audio.volume = 0.2;
+        audio.play();
+
         lastMap++;
     } else if (lastMap == maps.length - 1) {
         lastMap = 0;
         var url = maps[lastMap].blurImage
         var miniatura = maps[lastMap].image
+        var src = maps[lastMap].soundEffect;
         document.body.style.width = "100%";
         document.body.style.background = "url( " + url + ") no-repeat center center fixed";
         document.getElementById("mapimg").src = miniatura;
         document.body.style.backgroundSize = "cover";
+
+        var sound = document.getElementById('sound');
+        htmlString = "<audio " + "id='mapsound' " + "src=" + src + "></audio>";
+        sound.insertAdjacentHTML('beforeend', htmlString);
+        audio = document.getElementById('mapsound');
+        audio.load();
+        audio.volume = 0.2;
+        audio.play();
     }
 }
 
 function prev() {
-
     var audio = document.getElementById('audioPassLevel');
+    document.getElementById('sound').innerHTML = "";
     audio.volume = 0.2;
     audio.load();
     audio.play();
 
     if (lastMap == 0) {
-        console.log('inicio vamonos a devolvernos al final')
+
         lastMap = maps.length - 1;
         var url = maps[lastMap].blurImage;
         var miniatura = maps[lastMap].image;
+        var src = maps[lastMap].soundEffect;
         document.body.style.width = "100%";
         document.body.style.background = "url( " + url + ") no-repeat center center fixed";
         document.getElementById("mapimg").src = miniatura;
         document.body.style.backgroundSize = "cover";
+
+        var sound = document.getElementById('sound');
+        htmlString = "<audio " + "id='mapsound' " + "src=" + src + "></audio>";
+        sound.insertAdjacentHTML('beforeend', htmlString);
+        audio = document.getElementById('mapsound');
+        audio.load();
+        audio.volume = 0.2;
+        audio.play();
+
     } else {
-        lastMap = lastMap - 1;
+        lastMap--;
         var url = maps[lastMap].blurImage
         var miniatura = maps[lastMap].image
+        var src = maps[lastMap].soundEffect;
         document.body.style.width = "100%";
         document.body.style.background = "url( " + url + ") no-repeat center center fixed";
         document.getElementById("mapimg").src = miniatura;
         document.body.style.backgroundSize = "cover";
+
+        var sound = document.getElementById('sound');
+        htmlString = "<audio " + "id='mapsound' " + "src=" + src + "></audio>";
+        sound.insertAdjacentHTML('beforeend', htmlString);
+        audio = document.getElementById('mapsound');
+        audio.load();
+        audio.volume = 0.2;
+        audio.play();
     }
 }
 
 function playGame() {
-    var backgroundAudio = document.getElementById('#BetterRun');
+    var backgroundAudio = document.getElementById('BetterRun');
     var audio = document.getElementById('audioPlay');
     audio.play();
-    console.log(backgroundAudio)
     var millisecondsToWait = 2000;
     $(document).ready(function() {
         $('body').addClass('vanish')
