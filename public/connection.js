2dc8835628c0bc1be0629812b128e5f07a82d4ae
Roberto <rmacuna@uninorte.edu.co>
2017-11-21 16:20:44 -0500

    Conectado para escoger el mapa y el modo de juego

--- public/connection.js
+++ public/connection.js
@@ -3,8 +3,12 @@
   var usernames = [];
   var username;
   var myConection;
+  var finalModal;
+  var selectedModals = []; //Los tipos seleccionados por todos los usuarios
   let json;
   var inTheGame = false;
+
+
 
   var config = {
       apiKey: "AIzaSyAr7Re6XSMbsoFodWhxo4qkN59ycRlcJx8",
@@ -18,16 +22,35 @@
 
   // Sending data 
 
+
+  var ocurrencesMap = [];
+  var ocurrencesGameMode = [];
+
   var connectedUsers;
   var UserConectionRef = firebase.database().ref('Conections/');
   var players = firebase.database().ref('Players/');
+  var gameinfo = firebase.database().ref('GameInfo/');
 
+// Funcion que escucha cuando cambian datos en los usuarios conectados. 
   UserConectionRef.on('value', data);
   function data(data) {
       connectedUsers = data.val();
-      console.log(connectedUsers)
-
+      console.log(connectedUsers);
   }
+
+//Funcion que escucha cuando cambian datos en la info del juego
+  gameinfo.on('value', function (snapshot) {
+      snapshot.forEach(function(childSnapshot){
+        var str = JSON.stringify(childSnapshot.val());
+        let info = JSON.parse(str);
+        var mapname = info["map"];
+
+        var gamemode = info["modal"];
+        ocurrencesMap.push(mapname);
+        ocurrencesGameMode.push(gamemode);
+
+      });
+  });
   // Deteccion de cambios
   // Funcion que se ejecuta cada cierto tiempo.
 
@@ -41,17 +64,21 @@
   // }, 500);
 
   UserConectionRef.on("value", function(snapshot) {
-      if (connectedUsers == 1) {
+      if (connectedUsers == 2) {
           firebase.database().ref('Players/').once('value', function(snapshot) {
               snapshot.forEach(function(childSnapshot) {
                   var str = JSON.stringify(childSnapshot.val());
                   json = JSON.parse(str);
                   selectedmaps.push(json["map"]);
+                  selectedModals.push[json["modal"]];
                   usernames.push(json["username"]);
                   showConection(json["username"], " se ha unido a la partida");
+
               });
           });
-          finalmap = selectedmaps[Math.floor(Math.random() * selectedmaps.length - 1)]; //Un mapa random entre los elegidos
+          finalModal = selectedModals[Math.floor(Math.random() * selectedModals.length - 1)];
+          console.log(finalmap);
+          // pushGameInfo(finalmap, usernames, finalModal);
           initGame();
       } else {
           players.once("value", function(snapshot) {
@@ -67,7 +94,6 @@
       }
   });
 
-
   function showConection(user, message) {
       var snackbar = document.getElementById("snackbar");
       snackbar.innerText = user + message;
@@ -75,32 +101,75 @@
       setTimeout(function() { snackbar.className = snackbar.className.replace("show", ""); }, 3000);
   }
 
-
-  function pushInformation() {
-      inTheGame = true;
-      username = document.getElementById('username').value;
-      $('#user_input').addClass('disabled');
-      $('#btn-play').addClass('disabled');
-      document.getElementById('btnPrev').disabled = true;
-      document.getElementById('btnNext').disabled = true;
-      UserConectionRef.on('value', function(snapshot) {
-          myConection = snapshot.val();
-      })
-      loadInfo(maps[lastMap].name, characterNames[lastChar], username);
+  function showMessage(message) {
+      var snackbar = document.getElementById("snackbar");
+      snackbar.innerText = message;
+      snackbar.className = "show";
+      setTimeout(function() { snackbar.className = snackbar.className.replace("show", ""); }, 3000);
   }
 
-  function loadInfo(URL, charName, usname) {
+  // Corregir el unload porque cuando se mete al juego nos saca a todos.
+
+  function pushInformation(modal) {
+      if (modal == 'B') {
+          username = document.getElementById('username').value;
+          if (username.trim().length > 0) {
+              inTheGame = true;
+              $('#user_input').addClass('disabled');
+              $('#btn-play').addClass('disabled');
+              $('#btn-blocks').addClass('disabled');
+              document.getElementById('btnPrev').disabled = true;
+              document.getElementById('btnNext').disabled = true;
+              UserConectionRef.on('value', function(snapshot) {
+                  myConection = snapshot.val();
+              })
+              loadInfo(maps[lastMap].name, characterNames[lastChar].name, username, "BombTag");
+          } else {
+              showMessage("Debe ingresar un nombre de usuario");
+          }
+
+      } else if (modal == 'F') {
+          username = document.getElementById('username').value;
+          if (username.trim().length != 0) {
+              inTheGame = true;
+              $('#user_input').addClass('disabled');
+              $('#btn-play').addClass('disabled');
+              $('#btn-blocks').addClass('disabled');
+              document.getElementById('btnPrev').disabled = true;
+              document.getElementById('btnNext').disabled = true;
+              UserConectionRef.on('value', function(snapshot) {
+                  myConection = snapshot.val();
+              })
+              loadInfo(maps[lastMap].name, characterNames[lastChar].name, username, "FallingBlocks");
+          } else {
+              showMessage("Debe ingresar un nombre de usuario");
+          }
+      }
+
+  }
+
+  function loadInfo(URL, charName, usname, modality) {
       connectedUsers++;
       firebase.database().ref('Players/' + (connectedUsers)).set({
           map: URL,
           char: charName,
-          username: usname
+          username: usname,
+          modal: modality
       });
+
+      firebase.database().ref('GameInfo/' + (connectedUsers)).set({
+        map: URL,
+        modal: modality
+      });
+
       UserConectionRef.set(connectedUsers);
   }
+
+
+
   //Ahora detectaremos cuando alguien se salga de la sala de espera.
   window.onbeforeunload = function() {
-      if (inTheGame) {
+      if (inTheGame && connectedUsers < 3) {
           players.child(myConection).remove();
           connectedUsers--;
           UserConectionRef.set(connectedUsers);
