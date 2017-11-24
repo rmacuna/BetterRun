c0d8e2bfda003985d966d5bb5be758f7acc47539
Roberto <rmacuna@uninorte.edu.co>
2017-11-24 09:48:46 -0500

    Boton repetir sirve

--- public/reloby.js
+++ public/reloby.js
@@ -10,17 +10,36 @@
 
 
   var dbRef = firebase.database().ref('winner/');
-  var winner;
+  var UserConectionRef = firebase.database().ref('Conections/');
+  var players = firebase.database().ref('Players/');
+  var dbRefMapDesert = firebase.database().ref('selectedMaps/Desierto/');
+  var dbRefMapBosque = firebase.database().ref('selectedMaps/Bosque/');
+  var dbRefMapIce = firebase.database().ref('selectedMaps/CuevaHielo/');
+  var dbRefMapEspacio = firebase.database().ref('selectedMaps/Espacio/');
+  var dbRefMapLibertalia = firebase.database().ref('selectedMaps/Libertalia/');
+  var dbRefMapCementerio = firebase.database().ref('selectedMaps/Cementerio/');
+  var dbRefFallingB = firebase.database().ref('FallingBlocks/');
+  var dbRefBombTag = firebase.database().ref('BombTag/');
+  var dbRefFinalMap = firebase.database().ref('finalmap/');
+  var dbRefFinalMode = firebase.database().ref('finalmodal/');
 
-  dbRef.on('value', function (snapshot) {
-    winner = snapshot.val();
-    document.getElementById('win').innerText = "El ganador es " + winner;
+  dbRef.on('value', function(snapshot) {
+      winner = snapshot.val();
+      document.getElementById('win').innerText = "El ganador es " + winner;
   });
-
-
-
-
-
-
-  
-
+  function reset() {
+      dbRefFinalMap.set("");
+      dbRefMapIce.set(0);
+      dbRefFallingB.set(0);
+      dbRefBombTag.set(0);
+      dbRefMapBosque.set(0);
+      dbRefMapEspacio.set(0);
+      dbRefMapLibertalia.set(0);
+      dbRef.set("");
+      dbRefFinalMode.set("");
+      dbRefMapDesert.set(0);
+      UserConectionRef.set(0);
+      dbRefMapCementerio.set(0);
+      players.remove();
+      window.location.href = "index.html";
+  }
