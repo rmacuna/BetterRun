28fcfdcf30cf6bf6b3751bfa7068ebfacc698fb9
Roberto <rmacuna@uninorte.edu.co>
2017-11-24 00:52:15 -0500

    merge

--- server.js
+++ server.js
@@ -75,6 +75,12 @@ function newConnection(socket){
 	function bomb(data){
 		socket.broadcast.emit('bomb',data);
 	}
+
+	socket.on("newBomb",bomb);
+	function bomb(id){
+		console.log(id);
+	}
+
 	socket.on('block',block);
 	function block(data){
 		io.sockets.emit('block',data);
